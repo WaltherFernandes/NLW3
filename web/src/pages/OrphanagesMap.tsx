31b4f8202e7d1802ecc0633e34306387b1c0f314
Walther Fernandes <waltheraaf@gmail.com>
2020-10-14 19:23:26 -0300

    Envio exemplo cota

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import Token from '../token';
@@ -9,6 +10,15 @@ import Token from '../token';
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap(){
     return(
@@ -31,7 +41,7 @@ function OrphanagesMap(){
 
             <Map 
                 center={[-19.9415079,-43.4799773]}
-                zoom={14}
+                zoom={15}
                 style={{ width:'100%', height:'100%' }}
             >
                 {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
@@ -39,9 +49,22 @@ function OrphanagesMap(){
                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${Token}`}
                 />
 
+                <Marker 
+                    icon={mapIcon}
+                    position={[-19.9415079,-43.4799773]} 
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <a href="orphanages/1">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </a>
+                    </Popup>
+                </Marker>
+                
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
             </Link>
 
